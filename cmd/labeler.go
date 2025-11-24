bfa72e46b3ce8b9f03addba0b83498c926492c88
srz_zumix <zumix.cpp@gmail.com>
2025-11-25 00:57:25 +0900

    fix load config ref (#74)
    * fix load config ref
    
    * fix

--- cmd/labeler.go
+++ cmd/labeler.go
@@ -59,8 +59,10 @@ func NewLabelerCmd() *cobra.Command {
 				if err != nil {
 					return fmt.Errorf("failed to parse config path: %w", err)
 				}
-				if contentPaths.Ref != nil {
-					ref = *contentPaths.Ref
+				if contentPaths.Ref == nil {
+					if contentPaths.Repo == nil {
+						contentPaths.Ref = &ref
+					}
 				}
 				if contentPaths.Repo == nil {
 					contentPaths.Repo = &repository
